13e5d32fc7e9d03b5a8fed7a59f10bf0a32ed87c
aaccurso <accurso.alan@gmail.com>
2016-02-27 20:33:57 -0300

    Finishes server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import makeStore from './src/store';
 import startServer from './src/server';
+import {actions} from './src/reducer';
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: actions.SET_ENTRIES,
+  entries: require('./entries.json')
+});
+store.dispatch({
+  type: actions.NEXT
+});
