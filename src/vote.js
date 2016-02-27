85eaeed8eb5e844c8414aafc3c92ab544a401a67
aaccurso <accurso.alan@gmail.com>
2016-02-27 15:56:33 -0300

    Implements next reducer

--- src/vote.js
+++ src/vote.js
@@ -1,5 +1,15 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 export function setEntries (currentState, entries) {
   return currentState.set('entries', List(entries));
+}
+
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({
+      pair: entries.take(2)
+    }),
+    entries: entries.skip(2)
+  });
 }
--- test/vote.spec.js
+++ test/vote.spec.js
@@ -1,9 +1,10 @@
 import {expect} from 'chai';
 import {List, Map} from 'immutable';
 
-import {setEntries} from '../src/vote';
+import {setEntries, next} from '../src/vote';
 
 describe('Vote application', () => {
+
   it('should set entries', () => {
     const state = Map();
     const entries = ['Trainspotting', '28 Days Later'];
@@ -13,4 +14,23 @@ describe('Vote application', () => {
       entries: List.of('Trainspotting', '28 Days Later')
     }));
   });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later')
+        }),
+        entries: List.of('Sunshine')
+      }));
+    });
+
+  });
+
 });
