7edd3a1b7c710a0cd8ef57d96eb654f862682177
aaccurso <accurso.alan@gmail.com>
2016-02-27 19:31:11 -0300

    Future-proofing state

--- src/vote.js
+++ src/vote.js
@@ -18,9 +18,9 @@ export function next(state) {
   const entries = state.get('entries')
     .concat(getWinners(state.get('vote')));
   if (entries.size === 1) {
-    return Map({
-      winner: entries.first()
-    });
+    return state.remove('vote')
+      .remove('entries')
+      .set('winner', entries.first());
   }
   return state.merge({
     vote: Map({
